d648a37215d1ebd380e994cf437ecfef01ab26a4
Morais Domingos <141407881+CHINguli80@users.noreply.github.com>
2023-11-02 22:08:32 +0000

    migrations

--- src/server/database/migrations/0000_create_city.ts
+++ src/server/database/migrations/0000_create_city.ts
@@ -3,9 +3,9 @@ import { ETableNames } from '../ETableNames'
 
 export async function up(knex: Knex) {
   
-  return knex.schema.createTable('citys', table => {
+  return knex.schema.createTable(ETableNames.citys, table => {
     table.bigIncrements('id').primary().index()
-    table.string(ETableNames.citys, 150).index().notNullable()
+    table.string('name', 150).index().notNullable()
 
     table.comment('Tabela usada para armazenar cidades da tabela')
   }).then(() => {
